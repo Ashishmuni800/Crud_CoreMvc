79c53e5fb35e19bb449bea45b93b568bcc37fc54
Ashish <ashishmunigupta100@gmail.com>
2022-06-07 00:27:17 +0530

    Add project files.

---
+++ Crud_CoreMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crud_CoreMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_CoreMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var data = dbContext.employees.ToList();
+            return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.employees.Add(e);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var del = dbContext.employees.SingleOrDefault(e => e.Id == id);
+            dbContext.employees.Remove(del);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var ed = dbContext.employees.SingleOrDefault(e => e.Id == id);
+            return View(ed);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee e)
+        {
+            dbContext.employees.Update(e);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Crud_CoreMvc/Migrations/20220602205125_m2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crud_CoreMvc.Migrations
+{
+    public partial class m2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Mobile",
+                table: "employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
